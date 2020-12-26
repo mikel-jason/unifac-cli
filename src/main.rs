bfa5f971aa8e8b50a02ff228ef2c7d69e73fa52b
Mikel Muennekhoff <mmuennekhoff+github@gmail.com>
2020-12-26 12:29:23 +0100

    Auto-formatting

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::collections::HashMap;
 use std::fs;
 
-use serde_derive::{Serialize, Deserialize};
+use serde_derive::{Deserialize, Serialize};
 use unifac::{calc, FunctionalGroup, Substance};
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -24,22 +24,30 @@ fn run() -> Result<(), &'static str> {
     let filecontent = fs::read_to_string("assets/demo.yaml").unwrap();
     let content: YamlBody = serde_yaml::from_str(&filecontent).unwrap();
 
-    let substances = content.substances.iter().map(|(n, s)| {
-        let g = s.groups.iter().map(|g| {
-            let data: Vec<&str> = g.split(":").collect();
-            let id = str::parse::<u8>(data[0]).unwrap();
-            let count = str::parse::<f64>(data[1]).unwrap();
-            FunctionalGroup::from(id, count)
-        }).collect::<Result<Vec<FunctionalGroup>, &'static str>>()?;
-        Ok(Substance {
-            fraction: s.fraction,
-            functional_groups: g,
-            gamma: None,
+    let substances = content
+        .substances
+        .iter()
+        .map(|(n, s)| {
+            let g = s
+                .groups
+                .iter()
+                .map(|g| {
+                    let data: Vec<&str> = g.split(":").collect();
+                    let id = str::parse::<u8>(data[0]).unwrap();
+                    let count = str::parse::<f64>(data[1]).unwrap();
+                    FunctionalGroup::from(id, count)
+                })
+                .collect::<Result<Vec<FunctionalGroup>, &'static str>>()?;
+            Ok(Substance {
+                fraction: s.fraction,
+                functional_groups: g,
+                gamma: None,
+            })
         })
-    }).collect::<Result<Vec<Substance>, &'static str>>()?;
+        .collect::<Result<Vec<Substance>, &'static str>>()?;
 
     let mix = calc(substances, content.temperature)?;
     println!("{:?}", mix);
-    
+
     return Ok(());
 }
