98ce724d8e83e2621858afde67844c280ceb584d
Heringer <heringer@protonmail.com>
2020-12-28 13:43:13 +0100

    Added help text and output as yaml

--- README.md
+++ README.md
@@ -4,11 +4,11 @@ CLI application utilizing the [unifac crate](https://github.com/sarcaustech/unif
 `unifac` crate is used as local dependency. Therefore, both projects are assumed to be located in the same parent directory. Crate is referenced with `../unifac`.
 
 # Usage (PoC)
-Either edit [assets/demo.yaml](assets/demo.yaml) or add own file and change file path in [src/main.rs](src/main.rs). YAML structure is excected as:
+Program takes a YAML file as its input. YAML structure is excected as:
 ```yaml
 temperature: 298 # temperature in Kelvin
 substances: # array
-  ethanole: # substance name (not used, only for readability)
+  ethanole: # substance name (used only in the result for readability)
     fraction: 0.5 # fraction of substance in mixture
     groups: # array of functional groups in current substance
       - "1:1" # formatted as "<GROUP ID>:<COUNT OF GROUP IN SUBSTANCE>"
@@ -20,3 +20,6 @@ substances: # array
       - "9:6"
 ```
 For group IDs, see [used data source](http://www.ddbst.com/published-parameters-unifac.html).
+The program itself can be called using:
+```unifac-cli [OPTIONS] <YAML-FILE>```
+The output will be printed to stdout, unless a output file is set with `-o`.
--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,8 @@ use std::fs;
 use serde_derive::{Deserialize, Serialize};
 use unifac::{calc, FunctionalGroup, Substance};
 
+use clap::{App, Arg};
+
 #[derive(Debug, Serialize, Deserialize)]
 struct YamlBody {
     temperature: f64,
@@ -17,39 +19,84 @@ struct YamlSubstance {
 }
 
 fn main() {
-    let filecontent = fs::read_to_string("assets/demo.yaml").unwrap();
-    run(&filecontent);
+    let matches = App::new(env!("CARGO_PKG_NAME"))
+        .version(env!("CARGO_PKG_VERSION"))
+        .author(env!("CARGO_PKG_AUTHORS"))
+        .about("CLI for the UNIFAC library")
+        .arg(
+            Arg::with_name("output")
+                .short("o")
+                .long("output")
+                .value_name("FILE")
+                .help("Specifies output file")
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("input")
+                .help("Specifies input file")
+                .required(true)
+                .index(1),
+        )
+        .get_matches();
+
+    let filecontent = match fs::read_to_string(matches.value_of("input").unwrap()) {
+        Ok(c) => c,
+        Err(_) => {
+            eprintln!("Input file could not be read");
+            return;
+        }
+    };
+    let output = match run(&filecontent) {
+        Ok(o) => o,
+        Err(s) => {
+            eprintln!("{}\n", s);
+            return;
+        }
+    };
+    if matches.is_present("output") {
+        match fs::write(matches.value_of("output").unwrap(), output) {
+            Ok(_) => return,
+            Err(_) => eprintln!("The file could not be written"),
+        };
+    } else {
+        println!("{}", output);
+    }
 }
 
-fn run(yaml_str: &str) -> Result<(), &'static str> {
-    let content: YamlBody = serde_yaml::from_str(&yaml_str).unwrap();
+fn run(yaml_str: &str) -> Result<String, &'static str> {
+    let content: YamlBody = match serde_yaml::from_str(&yaml_str) {
+        Ok(c) => c,
+        Err(_) => return Err("Invalid syntax in input file!"),
+    };
 
     let substances = content
         .substances
         .iter()
-        .map(|(n, s)| {
-            let g = s
+        .map(|(name, substance)| {
+            let groups = substance
                 .groups
                 .iter()
-                .map(|g| {
-                    let data: Vec<&str> = g.split(":").collect();
-                    let id = str::parse::<u8>(data[0]).unwrap();
-                    let count = str::parse::<f64>(data[1]).unwrap();
+                .map(|group| {
+                    let data: Vec<&str> = group.split(":").collect();
+                    let id = match str::parse::<u8>(data[0]) {
+                        Ok(i) => i,
+                        Err(_) => return Err("Invalid syntax in input file!"),
+                    };
+                    let count = match str::parse::<f64>(data[1]) {
+                        Ok(i) => i,
+                        Err(_) => return Err("Invalid syntax in input file!"),
+                    };
                     FunctionalGroup::from(id, count)
                 })
                 .collect::<Result<Vec<FunctionalGroup>, &'static str>>()?;
-            Ok(Substance {
-                fraction: s.fraction,
-                functional_groups: g,
-                gamma: None,
-            })
+            Ok(Substance::from_name(name, substance.fraction, groups))
         })
         .collect::<Result<Vec<Substance>, &'static str>>()?;
 
     let mix = calc(substances, content.temperature)?;
-    println!("{:?}", mix);
+    let yaml_string = serde_yaml::to_string(&mix).unwrap();
 
-    return Ok(());
+    return Ok(yaml_string);
 }
 
 #[cfg(test)]
